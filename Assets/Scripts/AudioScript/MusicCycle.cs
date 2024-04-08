45f69a158e94881286550815213698bef0c859f8
MitchelBroekate <mitchelb11@outlook.com>
2024-04-08 13:38:44 +0200

    music

---
+++ Assets/Scripts/AudioScript/MusicCycle.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicCycle : MonoBehaviour
+{
+    public AudioClip song1;
+    public AudioClip song2;
+    public AudioClip song3;
+    public AudioClip song4;
+    public AudioSource source;
+
+    private void Update()
+    {
+       
+    }
+}
