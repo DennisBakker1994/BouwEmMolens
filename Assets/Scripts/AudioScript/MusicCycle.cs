0c5c169882a5ac55a14d5c82833d5be1bfd10282
MitchelBroekate <mitchelb11@outlook.com>
2024-04-08 13:50:59 +0200

    musicCycle

--- Assets/Scripts/AudioScript/MusicCycle.cs
+++ Assets/Scripts/AudioScript/MusicCycle.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MusicCycle : MonoBehaviour
@@ -9,9 +10,47 @@ public class MusicCycle : MonoBehaviour
     public AudioClip song3;
     public AudioClip song4;
     public AudioSource source;
+    private bool songIsPlaying;
 
+    int songI;
     private void Update()
     {
-       
+       if (!source.isPlaying)
+        {
+            songIsPlaying = false;
+            songI = Random.Range(0, 4);
+        }
+        if (!songIsPlaying)
+        {
+            ChangeSong();
+        }
+    }
+
+    void ChangeSong()
+    {
+        if(songI == 1)
+        {
+            source.clip = song1;
+            source.Play();
+            songIsPlaying = true;
+        }
+        if(songI == 2) 
+        {
+            source.clip = song2;
+            source.Play();
+            songIsPlaying = true;
+        }
+        if (songI == 3)
+        {
+            source.clip = song3;
+            source.Play();
+            songIsPlaying = true;
+        }
+        if (songI == 4)
+        {
+            source.clip = song4;
+            source.Play();
+            songIsPlaying = true;
+        }
     }
 }
