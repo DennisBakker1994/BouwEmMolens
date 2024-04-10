9c5a96b98c0a42d960c7d99cfdb49a2429eaaa6a
MitchelBroekate <mitchelb11@outlook.com>
2024-04-10 14:28:00 +0200

    music

--- Assets/Scripts/AudioScript/MusicCycle.cs
+++ Assets/Scripts/AudioScript/MusicCycle.cs
@@ -10,20 +10,16 @@ public class MusicCycle : MonoBehaviour
     public AudioClip song3;
     public AudioClip song4;
     public AudioSource source;
-    private bool songIsPlaying;
 
     int songI;
     private void Update()
     {
        if (!source.isPlaying)
         {
-            songIsPlaying = false;
-            songI = Random.Range(0, 4);
+            songI = Random.Range(1, 5);
 
-            if (!songIsPlaying)
-            {
-                ChangeSong();
-            }
+            ChangeSong();
+
         }
     }
 
@@ -33,25 +29,21 @@ public class MusicCycle : MonoBehaviour
         {
             source.clip = song1;
             source.Play();
-            songIsPlaying = true;
         }
         if(songI == 2) 
         {
             source.clip = song2;
             source.Play();
-            songIsPlaying = true;
         }
         if (songI == 3)
         {
             source.clip = song3;
             source.Play();
-            songIsPlaying = true;
         }
         if (songI == 4)
         {
             source.clip = song4;
             source.Play();
-            songIsPlaying = true;
         }
     }
 }
