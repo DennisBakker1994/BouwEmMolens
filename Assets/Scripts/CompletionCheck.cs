8859cd329183c7be3d5f2aa6ca8eb455d0647e44
MitchelBroekate <mitchelb11@outlook.com>
2024-04-11 11:07:04 +0200

    wieken animation en sounds

--- Assets/Scripts/CompletionCheck.cs
+++ Assets/Scripts/CompletionCheck.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class CompletionCheck : MonoBehaviour
 {
 
+    public AudioSource source;
+    public AudioClip clip;
+    public Animator animator;
+
     public void CheckForCompletion()
     {
         if (GetComponentInChildren<SnapManager>().windmillCompleted == true)
         {
             Debug.Log("1");
+            source.clip = clip;
+            source.Play();
+
+            animator.Play("New State 0");
+
+
         }
     }
 }
