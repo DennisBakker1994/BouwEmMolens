5c9a8e453881894a56d55fe523d4f3039c0b5cee
MitchelBroekate <mitchelb11@outlook.com>
2024-04-11 14:51:47 +0200

    placement stuff

--- Assets/Scripts/AudioScript/PlacementSoundAndParticle.cs
+++ Assets/Scripts/AudioScript/PlacementSoundAndParticle.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlacementSound : MonoBehaviour
+public class PlacementSoundAndParticle : MonoBehaviour
 {
     public AudioSource source;
     public AudioClip clip;
+    public ParticleSystem ps;
 
     private void OnCollisionEnter(Collision collision)
     {
         source.clip = clip;
         source.Play();
+
+        ps.Play();
     }
 }
